6486b93288ce4b3024ee77340e84220fcb5bd933
shss1105 <kimseung2331@naver.com>
2024-02-26 16:30:23 +0900

    Feat - 캐릭터 이동
    PlayerInputControlelr : 값 입력받기
    TopDownCharacterController
    : 구독 된 이벤트 호출
    TopDownMovement : 이동 처리

---
+++ Assets/Scripts/Controllers/TopDownCharacterController.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopDownCharacterController : MonoBehaviour
+{
+    public event Action<Vector2> OnMoveEvent;
+    public event Action<Vector2> OnLookEvent;
+
+    //호출 할 수 있는 함수
+    public void CallMoveEvent(Vector2 direction)
+    {
+        OnMoveEvent?.Invoke(direction);
+    }
+
+    
+    public void CallLookEvent(Vector2 direction)
+    {
+        OnLookEvent?.Invoke(direction);
+    }
+}
